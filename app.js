845f0f209f1990be6d62459fafd67dea46f9a66b
abbos91 <abbos.shomurodov91@gmail.com>
2022-03-24 18:01:59 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const form = document.querySelector("form");
+const input = document.getElementById("input");
+const error = document.querySelector("small");
+const validateEmail = (email) => {
+  return email.match(
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  );
+};
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (input.value === "") {
+    showError();
+  } else if (!validateEmail(input.value)) {
+    showError();
+  }
+});
+function showError() {
+  error.classList.add("visible");
+  input.classList.add("visible");
+  input.placeholder = "example@email/com";
+}
